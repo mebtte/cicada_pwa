451f6216ed1266cb616a935bfd19c99afc6bfd6e
mebtte <hi@mebtte.com>
2021-05-21 16:21:04 +0800

    improve about page header

--- src/pages/about/header.tsx
+++ src/pages/about/header.tsx
@@ -8,6 +8,10 @@ import { User } from '@/constants/user';
 import { ROOT_PATH } from '@/constants/route';
 
 const Style = styled.div`
+  z-index: 1;
+  position: sticky;
+  top: 0;
+  background: #fff;
   box-shadow: 0px 2px 4px -1px rgba(49, 194, 124, 0.2),
     0px 4px 5px 0px rgba(49, 194, 124, 0.14),
     0px 1px 10px 0px rgba(49, 194, 124, 0.12);
