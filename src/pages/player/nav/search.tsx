e1db1307d986d7c5d89f82d21b5538a02e891cf0
mebtte <hi@mebtte.com>
2021-10-06 21:43:23 +0800

    search shortcut

--- src/pages/player/nav/search.tsx
+++ src/pages/player/nav/search.tsx
@@ -1,6 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
+import { IS_MAC_OS, IS_WINDOWS } from '@/constants';
+import keyboardHandlerWrapper from '@/utils/keyboard_handler_wrapper';
 import IconButton, { Name as IconButtonName } from '@/components/icon_button';
 import Input from '@/components/input';
 import toast from '@/platform/toast';
@@ -22,6 +24,8 @@ const Search = () => {
   const history = useHistory();
   const { searchWord } = useContext(Context);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const [keyword, setKeyword] = useState('');
   const onSearch = () => {
     const k = keyword || searchWord;
@@ -41,6 +45,20 @@ const Search = () => {
     }
   };
 
+  useEffect(() => {
+    const onKeydown = keyboardHandlerWrapper((event: KeyboardEvent) => {
+      if (
+        event.key === 'f' &&
+        ((IS_MAC_OS && event.metaKey) || (IS_WINDOWS && event.ctrlKey))
+      ) {
+        event.preventDefault();
+        inputRef.current.focus();
+      }
+    });
+    document.addEventListener('keydown', onKeydown);
+    return () => document.removeEventListener('keydown', onKeydown);
+  }, []);
+
   return (
     <Style>
       <Input
@@ -49,6 +67,7 @@ const Search = () => {
         onChange={onKeywordChange}
         onKeyDown={onKeyDown}
         placeholder={searchWord || '搜索'}
+        ref={inputRef}
       />
       <IconButton name={IconButtonName.SEARCH_OUTLINE} />
     </Style>
