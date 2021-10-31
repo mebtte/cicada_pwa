201b12faee667f17b7a4ad3d644572fef8e1f4ad
mebtte <hi@mebtte.com>
2021-10-31 18:20:01 +0800

    fix play mode persist

--- src/pages/player/use_play_mode.ts
+++ src/pages/player/use_play_mode.ts
@@ -4,14 +4,21 @@ import { PlayMode, PLAY_MODES } from './constants';
 import { PLAY_MODE } from '../../constants/storage_key';
 import eventemitter, { EventType } from './eventemitter';
 
+const getInitialPlayMode = () => {
+  const pm = localStorage.getItem(PLAY_MODE) as PlayMode;
+  return pm && PLAY_MODES.includes(pm) ? pm : PlayMode.SQ;
+};
+
 export default () => {
-  const [playMode, setPlayMode] = useState<PlayMode>(() => {
-    const pm = localStorage.getItem(PLAY_MODE) as PlayMode;
-    return pm && PLAY_MODES.includes(pm) ? pm : PlayMode.SQ;
-  });
+  const [playMode, setPlayMode] = useState<PlayMode>(getInitialPlayMode);
 
   useEffect(() => {
-    const playModeChangeListener = (pm: PlayMode) => setPlayMode(pm);
+    const playModeChangeListener = (pm: PlayMode) => {
+      setPlayMode(pm);
+      window.requestIdleCallback(() =>
+        window.localStorage.setItem(PLAY_MODE, pm),
+      );
+    };
     eventemitter.on(EventType.CHANGE_PLAY_MODE, playModeChangeListener);
     return () =>
       void eventemitter.off(EventType.CHANGE_PLAY_MODE, playModeChangeListener);
